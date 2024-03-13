e233d3462dc117dead122a3a6f0eeb4649c6ebfb
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2024-03-12 20:43:56 -0500

    reinicie los commmits por que no hice un pull antes y malogre todo, gg

---
+++ README.md
@@ -0,0 +1,11 @@
+# Buena SysVet - Frontend
+
+Enlace de vista:
+
+https://buena-sys-vet.onrender.com/
+
+
+```sh
+npm run dev
+```
+
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+//import Layout from "./components/layout";
+//import Home from "./pages/home";
+
+export default function App({ children }) {
+  return (<div>Hello!</div>);
+}
